6dd7a77c9a93ec35658632c89d6f4ea347389a98
Nicholas Pettas <npettas@gmail.com>
2023-12-19 18:50:59 -0800

    fix unit tests

--- integration/xenblocks/verifier/targets_test.go
+++ integration/xenblocks/verifier/targets_test.go
@@ -14,8 +14,19 @@ func TestFindTokensFromHashXNM(t *testing.T) {
 
 	assert.Equal(1, len(tokens))
 	assert.Equal("XNM", tokens[0].name)
-	assert.Equal(1, tokens[0].currencyCode)
+	assert.Equal(uint8(1), tokens[0].currencyCode)
 	assert.Equal(10, tokens[0].value)
+}
+
+func TestXuniToken(t *testing.T) {
+	assert := require.New(t)
+
+	tokens := FindTokensFromHash("$argon2id$v=19$m=102000,t=1,p=1$AmtN2N+5ggJ4tc1s7TfBsg9o3to$ZzzXErgy4F3+O4+OV2U3F9+Sv0ssKR4uyxguomjkBvb1EUgEeC5Ztyz2U7k9gKHw8e6HidQkKXUNI75ZFBmQHw", time.Unix(0, 0))
+
+	assert.Equal(1, len(tokens))
+	assert.Equal("XUNI", tokens[0].name)
+	assert.Equal(uint8(2), tokens[0].currencyCode)
+	assert.Equal(1, tokens[0].value)
 }
 
 func TestFindTokensFromHashXUNI(t *testing.T) {
@@ -29,7 +40,7 @@ func TestFindTokensFromHashXUNI(t *testing.T) {
 			if (0 <= j && j < 5) || (55 <= j && j < 60) {
 				assert.Equal(1, len(tokens))
 				assert.Equal("XUNI", tokens[0].name)
-				assert.Equal(2, tokens[0].currencyCode)
+				assert.Equal(uint8(2), tokens[0].currencyCode)
 				assert.Equal(1, tokens[0].value)
 			} else {
 				assert.Equal(0, len(tokens))
@@ -46,9 +57,9 @@ func TestFindTokensFromHashSB(t *testing.T) {
 	tokens := FindTokensFromHash("$argon2id$v=19$m=99400,t=1,p=1$salt$XEN11AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", blockTime)
 	assert.Equal(2, len(tokens))
 	assert.Equal("XNM", tokens[0].name)
-	assert.Equal(1, tokens[0].currencyCode)
+	assert.Equal(uint8(1), tokens[0].currencyCode)
 	assert.Equal(10, tokens[0].value)
 	assert.Equal("SB", tokens[1].name)
-	assert.Equal(3, tokens[1].currencyCode)
+	assert.Equal(uint8(3), tokens[1].currencyCode)
 	assert.Equal(1, tokens[1].value)
 }
