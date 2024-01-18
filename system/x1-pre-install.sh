e933609e1ec579efef18b532d68823df0ce11049
Nicholas Pettas <npettas@gmail.com>
2024-01-17 17:08:02 -0800

    wip

--- system/x1-pre-install.sh
+++ system/x1-pre-install.sh
@@ -3,7 +3,7 @@ set -e
 
 PKG="x1"
 
-if [ -x "$(command -v foo)" ]; then
+if [ -x "$(command -v useradd)" ]; then
 	if ! getent passwd $PKG >/dev/null ; then
 			useradd --system --user-group --create-home --home-dir /var/lib/$PKG $PKG
 			echo "Created system user $PKG"
