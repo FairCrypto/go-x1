d7f39a3eebf04db83fb9dc146209c41d3aad13a2
Nicholas Pettas <npettas@gmail.com>
2024-01-17 17:09:56 -0800

    wip

--- system/x1-post-install.sh
+++ system/x1-post-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -d /run/systemd/system ]; then
+if [ -d /run/systemd/system ] && [ -d /etc/systemd/system/ ]; then
+	cp system/lib/systemd/system/x1.service /etc/systemd/system/
 	systemctl daemon-reload
 	systemctl try-restart x1
 fi
