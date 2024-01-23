fa20f25c929e76d4a03f9deeb35adf8afc5ff641
Nicholas Pettas <npettas@gmail.com>
2024-01-23 10:38:59 -0800

    use env var for private key in the test script
    use env var for private key in the test script

--- docker/networksimulator/test.py
+++ docker/networksimulator/test.py
@@ -1,11 +1,12 @@
 from web3 import Web3, HTTPProvider
 from web3.middleware import construct_sign_and_send_raw_middleware
 from time import sleep, time
+import os
 
 # Connect to local Ethereum node
 w3 = Web3(HTTPProvider('http://localhost:8545'))
 
-pk = ""  # add private key here
+pk = os.environ.get('PRIVATE_KEY')  # add private key here
 acct1 = w3.eth.account.from_key(pk)
 
 w3.middleware_onion.add(construct_sign_and_send_raw_middleware(acct1))
@@ -15,7 +16,9 @@ while True:
         "from": acct1.address,
         "to": acct1.address,
         "value": 123123123123123,
-        'gas': 21000,
+        'gas': 22000,
+        "maxFeePerGas": Web3.to_wei(90, 'gwei'),
+        "maxPriorityFeePerGas": Web3.to_wei(200, 'gwei'),
     })
 
     start_time = time()
